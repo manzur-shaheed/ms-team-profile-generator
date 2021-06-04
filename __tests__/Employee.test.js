d927c47a4eb049a9edd1d4b80c63b95ddb5ffb21
ms <shaheed_manzur@yahoo.com>
2021-06-03 23:01:23 -0400

    Wrote first test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+// tests for Employee class
+const Employee = require('../lib/Employee');
+
+describe("Employee class", () => {
+    it("Testing Employee object", () => {
+        const employee = new Employee();
+        console.log(typeof(employee));
+        expect(typeof(employee)).toBe("object");
+    });
+});
